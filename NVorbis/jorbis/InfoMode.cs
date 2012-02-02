2d8ed016155bdb6324bcfdc637b835d98d56c45f
soywiz <soywiz@gmail.com>
2012-02-02 14:41:29 +0100

    - First commit

---
+++ NVorbis/jorbis/InfoMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NVorbis.jorbis
+{
+	internal class InfoMode
+	{
+		internal int blockflag;
+		internal int windowtype;
+		internal int transformtype;
+		internal int mapping;
+	}
+}
